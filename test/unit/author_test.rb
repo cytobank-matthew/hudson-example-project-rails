93464718738c33658ff8d176da162542f897e10e
Stefan Saasen <s@juretta.com>
2008-11-22 14:46:08 +1300

    Added invalid test.

--- test/unit/author_test.rb
+++ test/unit/author_test.rb
@@ -15,4 +15,9 @@ class AuthorTest < ActiveSupport::TestCase
     assert_equal "stefan", Author.find(:first).name
   end
   
+  def test_name_4
+    # This test will fail.
+    assert_equal "stefan2", Author.find(:first).name
+  end
+  
 end
